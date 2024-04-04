e6bfac0bfb097ddbeaf350d1f86814cf5a13336f
Alex <fornikaras7@gmail.com>
2024-04-04 17:46:54 +0500

    feat: finish getPowerFunction

--- src/functions-tasks.js
+++ src/functions-tasks.js
@@ -71,8 +71,10 @@ function getArgumentsCount(/* funcs */) {
  *   power05(16) => 4
  *
  */
-function getPowerFunction(/* exponent */) {
-  throw new Error('Not implemented');
+function getPowerFunction(exponent) {
+  return (...args) => {
+    return args ** exponent;
+  };
 }
 
 /**
